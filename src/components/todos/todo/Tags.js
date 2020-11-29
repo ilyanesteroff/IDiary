0d2a15343a05246af6667e8b3f2b5931d93736e3
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-11-29 19:20:31 +0200

    route fixing

--- src/components/todos/todo/Tags.js
+++ src/components/todos/todo/Tags.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 
 export default ({ tags }) => (
   <ul id="TodoTags">
     {tags.map((tag, index) => 
       <li key={'tag'+index}>
-        <Link to={`/tags/${tag}`}>
+        <a href={`/tags/${tag}`}>
           {'#'+tag}
-        </Link>
+        </a>
       </li>
     )}
   </ul>
