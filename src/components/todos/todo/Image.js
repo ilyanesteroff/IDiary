b5aea3f98d5fcdfcc83e191ee8a76bb9a12710e0
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-12-03 15:58:36 +0200

    added file picker field

--- src/components/todos/todo/Image.js
+++ src/components/todos/todo/Image.js
@@ -4,7 +4,7 @@ export default ({ imgSrc }) => (
   imgSrc
     ? <img 
         src={'https://todo-pic-bucket.s3.eu-north-1.amazonaws.com/' + imgSrc}
-        alt="An image"
+        alt=""
       />
     : null
 )
