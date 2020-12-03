47fdcb60fc1675d749f11840d0d631fdd25ea864
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-12-03 14:16:26 +0200

    added file picker field

--- src/utils/formatTodo.js
+++ src/utils/formatTodo.js
@@ -20,7 +20,8 @@ export default async todoData => {
   timeToComplete !== '' && timeToComplete !== '0'
     ? todoInput.timeToComplete =  parseFloat(timeToComplete).toPrecision(3) * hours.hour * 1000
     : todoInput.timeToComplete = null
-  console.log(todoData)
+
+  console.log(todoData.image)
   if(todoData.image && todoData.image !== 'remove'){
     const creds = await getPresignedUrl()
     const success = await loadPic(todoData.image, creds.url)
