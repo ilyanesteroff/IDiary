48d9d2e46126c1727764837cb93d443d48d431aa
Ilya Nesterov <j.nesterov1709@gmail.com>
2020-12-03 17:56:40 +0200

    added file picker field

--- src/components/FormComponents/ImageInput.js
+++ src/components/FormComponents/ImageInput.js
@@ -5,14 +5,14 @@ import RemoveImage from '../todos/todo/RemoveImage'
 export default ({ setImage, editing, file }) => {
   return(
   <>
-    <p>{ editing? 'Pick a new image' : 'Pick an image' }</p>
+    <p>{ editing ? 'Pick a new image' : 'Pick an image' }</p>
     <div id="RelativeInput">
       <input
         type="file"  
         accept="image/*" 
         onChange={e => setImage(e.target.files[0])}
       />
-      {editing && file && file !== 'remove' &&
+      {editing &&
         <RemoveImage clickHandler={_ => setImage('remove')}/>
       }
     </div>
